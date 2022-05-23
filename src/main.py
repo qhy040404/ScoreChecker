fd85ded167bbd37371cb3163339903d59c8cf124
qhy040404 <qhy040404@163.com>
2022-05-23 12:31:13 +0800

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,70 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+
+import sso
+
+userid = os.getenv("ID")
+passwd = os.getenv("PASSWD")
+solutionId = os.getenv("SID")
+mailId = os.getenv("MAIL_ID")
+mailPass = os.getenv("MAIL_PASS")
+notify = os.getenv("NOTIFICATION")
+
+scoreUrl = "http://jxgl.dlut.edu.cn/student/for-std/grade/sheet"
+solutionUrl = "http://jxgl.dlut.edu.cn/student/for-std/program-completion-preview"
+
+if solutionId is not None:
+    scoreUrl += "/info/" + solutionId + "?semester="
+    solutionUrl += "/info/" + solutionId
+
+s = sso.login(userid, passwd)
+
+
+def send_email():
+    mail_user = mailId
+    mail_pass = mailPass
+    sender = mailId
+    receiver = mailId
+    mail_temp_data = mail_user.split('@')
+    mail_host_pre = 'smtp.'
+    mail_host = mail_host_pre + mail_temp_data[1]
+
+    print('Sending email...')
+
+    context = notify + "出了，焯"
+
+    message = MIMEText(context, 'plain', 'utf-8')
+    message['Subject'] = '死亡降临'
+    message['From'] = sender
+    message['To'] = receiver
+
+    try:
+        smtpObj = smtplib.SMTP_SSL(mail_host, 465)
+        smtpObj.login(mail_user, mail_pass)
+        smtpObj.sendmail(sender, receiver, message.as_string())
+        smtpObj.quit()
+        print('Email succeed')
+    except smtplib.SMTPException as e:
+        print('Email error', e)
+
+
+def get_score():
+    originalData = s.get(scoreUrl)
+    if notify in originalData:
+        send_email()
+
+
+def get_solution():
+    originalData = s.get(solutionUrl)
+    if notify in originalData:
+        send_email()
+
+
+def main():
+    get_score()
+    get_solution()
+
+
+if __name__ == "__main__":
+    main()
